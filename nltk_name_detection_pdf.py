7e1b46a82e703dddd9faa75054148b5220ff48d7
ARNAB MUKHERJEE <119810904+arnabm-94@users.noreply.github.com>
2023-09-18 00:52:30 +0530

    Add files via upload

---
+++ nltk_name_detection_pdf.py
@@ -0,0 +1,35 @@
+import PyPDF2
+import nltk
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with open(pdf_path, 'rb') as pdf_file:
+        pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+        for page_num in range(pdf_reader.numPages):
+            page = pdf_reader.getPage(page_num)
+            text += page.extractText()
+    return text
+
+def extract_names(txt):
+    person_names = []
+
+    for sent in nltk.sent_tokenize(txt):
+        for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+            if hasattr(chunk, 'label') and chunk.label() == 'PERSON':
+                person_names.append(
+                    ' '.join(chunk_leave[0] for chunk_leave in chunk.leaves())
+                )
+
+    return person_names
+
+if __name__ == '__main__':
+    text = extract_text_from_pdf('Arnab.pdf')
+    names = extract_names(text)
+
+    if names:
+        print(names[0])
